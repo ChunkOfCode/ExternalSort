6a4058f06c5f3c45c40da9e6725b37f820f246f8
Рыбаков Илья Сергеевич <rybakov_is@atb.su>
2023-10-09 02:39:04 +1000

    Создать стартовый проект

---
+++ src/main/java/com/example/externalfilesort/ExternalFileSortApplication.java
@@ -0,0 +1,13 @@
+package com.example.externalfilesort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalFileSortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalFileSortApplication.class, args);
+    }
+
+}
